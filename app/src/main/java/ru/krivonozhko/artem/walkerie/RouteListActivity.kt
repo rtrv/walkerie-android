95fadf0abf7d8f0023fe748b26f0f2d691bdd140
rtrv <artemkrivonozhko@gmail.com>
2017-06-05 23:34:47 +0300

    Generate base application

---
+++ app/src/main/java/ru/krivonozhko/artem/walkerie/RouteListActivity.kt
@@ -0,0 +1,12 @@
+package ru.krivonozhko.artem.walkerie
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RouteListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_route_list)
+    }
+}
